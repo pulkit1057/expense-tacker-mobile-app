8dea6e89a5b6acd544dcb0ae848147186d7fc428
pulkit1057 <pulkati.garg@gmail.com>
2024-04-24 00:39:50 +0530

    flana dhimkana

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
-var example= "This is an uneccesary change";
 var kDarkColorTheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 5, 99, 125),
